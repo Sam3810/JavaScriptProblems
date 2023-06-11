692f9df9ce83a03b9cebf23b22f35a8ffcc10961
cocinero <118696492+Sam3810@users.noreply.github.com>
2023-06-11 18:57:39 -0500

    Add files via upload

---
+++ Problems/index.js
@@ -0,0 +1,63 @@
+//Sistema de baneos
+/*const arr1 = ['pepe', 'sofia', 'gabriela']
+const arr = ['jose', 'juan', 'miguel', arr1]
+
+
+
+for (let name of arr) {
+    if (name == arr1) {
+        for (let name of arr1) {
+            if (name == 'pepe') {
+                console.log('el usuario ' + name + ' esta baneado')
+                continue;
+            }
+            document.write(name + '<br>')
+        }
+    }else {
+        document.write(name + '<br>')
+    }
+}*/
+
+
+//Discoteca entradas
+/*let entrada = false;
+
+function entrar (hora) {
+    let edad = prompt('Cuantos años tienes?')
+    if (edad >= 18) {
+
+        if (hora >= 2 && hora <= 5 && entrada == false) {
+            entrada = true
+            document.write('Puedes pasar gratis <br>')
+        }else if (entrada == true) {
+            document.write('Puedes pasar por $5 <br>')
+        }else {
+            document.write('Esta cerrado')
+        }
+
+    }else {
+        document.write('Eres menor de edad vete o llamo a tus padres <br>')
+    }
+}*/
+
+//Inicio de sesion
+/*alert('hola');
+let usuario = prompt('Quien eres?');
+
+if (usuario == 'administrador') {
+    let contraseña = prompt('Contraseña?');
+
+    if (contraseña == 123) {
+        alert('Has iniciado sesion')
+    }else if (contraseña == "" || contraseña == null) {
+        alert('Se ha cancelado el inicio de sesion')
+    }else{
+        alert('Contraseña incorrecta')
+    }
+
+}else if (usuario == "" || usuario == null) {
+    alert('Se ha cancelado el inicio de sesion')
+}else {
+    alert('Usuario desconocido')
+}*/
+
