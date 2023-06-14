194c9dfe37527605c0c3291d37c66ac7facd2ba2
cocinero <118696492+Sam3810@users.noreply.github.com>
2023-06-13 20:13:25 -0500

    Add files via upload

--- Problems/index.js
+++ Problems/index.js
@@ -2,8 +2,6 @@
 /*const arr1 = ['pepe', 'sofia', 'gabriela']
 const arr = ['jose', 'juan', 'miguel', arr1]
 
-
-
 for (let name of arr) {
     if (name == arr1) {
         for (let name of arr1) {
@@ -40,6 +38,7 @@ function entrar (hora) {
     }
 }*/
 
+
 //Inicio de sesion
 /*let usuario = prompt('Hola, quien eres?');
 
@@ -60,36 +59,83 @@ if (usuario == 'administrador') {
     alert('Usuario desconocido')
 }*/
 
+
 //Dias de la semana narrados
-/*let day = prompt('Que dia es hoy?')
+/*let day = prompt('Que dia es hoy?').toLowerCase()
 
 switch(day) {
     case 'lunes':
-    case 'Lunes':
         alert('Que aburrido')
         break
     case 'martes':
-    case 'Martes':
         alert('Por lo menos no es lunes')
         break
     case 'miercoles':
-    case 'Miercoles':
         alert('Falta poco para descansar, tu puedes')
         break
     case 'jueves':
-    case 'Jueves':
         alert('Ya mañana es viernes, vamos que se puede')
         break
     case 'viernes':
-    case 'Viernes':
         alert('Largo dia de trabajo, pero lo has logrado a descansar 🛏')
         break
     case 'sabado':
-    case 'Sabado':
     case 'domingo':
-    case 'Domingo':
         alert('😴')
         break
     default:
         alert('Escribe bien')
 }*/
+
+
+//La cuenta del restaurante
+/*let precio, propina, resultado
+
+function preguntarCena () {
+
+    precio = parseFloat(prompt('Cuanto ha costado la comida?'))
+
+    if (typeof precio == 'number' && precio <= 0) {
+        alert('No has comido nada...')
+    }else if (typeof precio == 'number' && precio <= 10) {
+        alert('Ha sido barata la comida')
+        preguntarPropina()
+    }else if (typeof precio == 'number' && precio >= 10 && precio <= 20) {
+        alert('Te has comprado una buena cena')
+        preguntarPropina()
+    }else if (typeof precio == 'number' && precio >= 20 && precio <= 30) {
+        alert('Pero eres millonario o que!?')
+        preguntarPropina()
+    }else if (typeof precio == 'number' && precio >= 30) {
+        alert('No tenemos en nuestro menu alimentos mayores a $30')
+        preguntarCena()
+    }else {
+        alert('Ingrese un valor valido')
+        preguntarCena()
+    }
+}
+
+function preguntarPropina () {
+
+    propina = parseFloat(prompt('Cuanto deseas dejar de propina?'))
+
+    if (typeof propina == 'number' && propina <= 0) {
+        alert('Jodete 🖕')
+    }else if (typeof propina == 'number' && propina <= 10) {
+        alert('Gracias por ayudar al mesero')
+    }else if (typeof propina == 'number' && propina >= 10 && propina <= 20) {
+        alert('Pero que gentil eres 😊')
+    }else if (typeof propina == 'number' && propina >= 20 && propina <= 30) {
+        alert('Eres la persona mas humilde que he visto, te quiero ❤')
+    }else if (typeof propina == 'number' && propina >= 30) {
+        alert('No nos tienen permitido aceptar propinas tan altas')
+        preguntarPropina()
+    }else {
+        alert('Ingrese un valor valido')
+        preguntarPropina()
+    }
+}
+
+preguntarCena()
+let suma = precio + propina
+resultado = alert('El coste total de su comida es de $' + suma)*/
